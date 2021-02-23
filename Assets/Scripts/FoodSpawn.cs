82baee5d6669f039a21668e0db6fd1d494d3fece
arthura17 <arthura17@students.ecu.edu>
2021-02-23 16:26:23 -0500

    Food spawn
    Food spawns randomly
    
    Need to fix:
    Collision with animals and player

---
+++ Assets/Scripts/FoodSpawn.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodSpawn : MonoBehaviour
+{
+
+    public float spawnRangeX = 15;
+    public float spawnPosZ = 15;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+    }
+
+    public void spawnFood()
+    {
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, Random.Range(0, spawnPosZ));
+        Instantiate(gameObject, spawnPos, gameObject.transform.rotation);
+    }
+
+
+    /*public void OnColliderEnter(Collider other)
+    {
+        if (other.gameObject.tag == "food")
+        {
+            Debug.Log("Food!");
+            Destroy(other.gameObject);
+            spawnFood();
+            Debug.Log("Food!");
+        }
+    }*/
+
+    void OnTriggerEnter(Collider col)
+    {
+        spawnFood();
+        Debug.Log("Food!");
+        Destroy(gameObject);
+    }
+
+}
