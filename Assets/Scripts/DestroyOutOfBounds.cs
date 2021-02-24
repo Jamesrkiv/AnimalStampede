e9f8ae58599b946f76391a103c072299d3167ad6
James Kent <kentja18@students.ecu.edu>
2021-02-24 16:19:01 -0500

    Added animal spawning from all sides & added cows

--- Assets/Scripts/DestroyOutOfBounds.cs
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -6,6 +6,8 @@ public class DestroyOutOfBounds : MonoBehaviour
 {
     private float topBound = 30;
     private float lowerBound = -10;
+    private float rightBound = 25;
+    private float leftBound = -25;
 
     // Start is called before the first frame update
     void Start()
@@ -16,14 +18,13 @@ public class DestroyOutOfBounds : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.z > topBound)
+        if (transform.position.z > topBound || transform.position.z < lowerBound) // Deltes things that go off the top or bottom of the screen
         {
             Destroy(gameObject);
         }
-        else if (transform.position.z < lowerBound)
+        else if (transform.position.x < leftBound || transform.position.x > rightBound) // Deletes things that go off the sides of the screen
         {
             Destroy(gameObject);
-            Debug.Log("Game Over!");
         }
     }
 }
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     private float spawnRangeX = 20;
-    private float spawnPosZ = 20;
+    private float[] spawnPosZ = new float[2]; // Spawn positions on z-axis
+    private float[] spawnPosX = new float[2]; // Spawn positions on x-axis
+    
     private float startDelay = 2;
     private float spawnInterval = 1.5f;
 
@@ -14,6 +16,11 @@ public class SpawnManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        spawnPosZ[0] = -5;
+        spawnPosZ[1] = 20;
+        spawnPosX[0] = -22;
+        spawnPosX[1] = 22;
+
         InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
     }
 
@@ -30,8 +37,29 @@ public class SpawnManager : MonoBehaviour
 
     void SpawnRandomAnimal()
     {
-        int animalIndex = Random.Range(0, animalPrefabs.Length);
-        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
-        Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
+        if (Random.Range(0, 2) > 0) // Decides whether animal spawns from top/bottom or sides of screen
+        {
+            float zPos = spawnPosZ[Random.Range(0, 2)];
+            Quaternion rotation = new Quaternion();
+
+            if (zPos == spawnPosZ[0]) rotation = Quaternion.Euler(0f, 0f, 0f); // Corrects rotation of animal
+            else rotation = Quaternion.Euler(0f, 180f, 0f);
+
+            int animalIndex = Random.Range(0, animalPrefabs.Length);
+            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, zPos);
+            Instantiate(animalPrefabs[animalIndex], spawnPos, rotation);
+        }
+        else
+        {
+            float xPos = spawnPosX[Random.Range(0, 2)]; 
+            Quaternion rotation = new Quaternion();
+
+            if (xPos == spawnPosX[0]) rotation = Quaternion.Euler(0f, 90f, 0f); // Corrects rotation of animal
+            else rotation = Quaternion.Euler(0f, 270f, 0f);
+
+            int animalIndex = Random.Range(0, animalPrefabs.Length);
+            Vector3 spawnPos = new Vector3(xPos, 0, Random.Range(-1, 15));
+            Instantiate(animalPrefabs[animalIndex], spawnPos, rotation);
+        }
     }
 }
